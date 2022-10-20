ba1805e9cdfe48b693a452784215987f1aa48e42
Elaine <elainedoud@gmail.com>
2022-10-20 11:46:43 -0400

    Mock Code Challenge

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 // write your code here
+const makeRamen = document.querySelector("#ramen-menu")
+const ramenPic = document.createElement("img")
+
+fetch("http://localhost:3000/ramens")
+.then (res => res.json())
+.then (ramens => elem {
+    ramens.forEach => { 
+    ramenPic.innerHTML = elem.image
+    makeRamen.append(ramenPic)
+    console.log(makeRamen.append(ramenPic))
+    })
+)
+
+addEventListener("click",(e)) {
+    e.preventDefault()
+
+}
+
+const ramenPicture = e.target.image
